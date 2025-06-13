fdeb17e198909899b92b9f31bc578e9c20df0575
GANONDORF\CASPI <caspian@tylerinternet.com>
2025-06-13 15:44:30 -0400

    Probably didn't fix but at least made errors go away with refactored readonly props by using harmony access tools.

--- Source/Client/Saving/CacheForReloading.cs
+++ Source/Client/Saving/CacheForReloading.cs
@@ -1,8 +1,11 @@
 using HarmonyLib;
+using RimWorld;
 using RimWorld.Planet;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using UnityEngine;
 using Verse;
 
 namespace Multiplayer.Client
@@ -67,18 +70,33 @@ namespace Multiplayer.Client
 
             WorldGrid grid = __instance;
 
-            grid.viewAngle = copyFrom.viewAngle;
-            grid.viewCenter = copyFrom.viewCenter;
-            grid.verts = copyFrom.verts;
-            grid.tileIDToNeighbors_offsets = copyFrom.tileIDToNeighbors_offsets;
-            grid.tileIDToNeighbors_values = copyFrom.tileIDToNeighbors_values;
-            grid.tileIDToVerts_offsets = copyFrom.tileIDToVerts_offsets;
-            grid.averageTileSize = copyFrom.averageTileSize;
+            //grid.viewAngle = copyFrom.viewAngle;
+            AccessTools.Property(typeof(WorldGrid), "SurfaceViewAngle").SetValue(grid, copyFrom.SurfaceViewAngle);
+            //grid.viewCenter = copyFrom.viewCenter;
+            AccessTools.Property(typeof(WorldGrid), "SurfaceViewCenter").SetValue(grid, copyFrom.SurfaceViewCenter);
+            //grid.verts = copyFrom.verts;
+            AccessTools.Property(typeof(WorldGrid), "UnsafeVerts").SetValue(grid, copyFrom.UnsafeVerts);
 
-            grid.tiles = new List<Tile>();
+            //grid.tileIDToNeighbors_offsets = copyFrom.tileIDToNeighbors_offsets;
+            AccessTools.Property(typeof(WorldGrid), "UnsafeTileIDToNeighbors_offsets").SetValue(grid, copyFrom.UnsafeTileIDToNeighbors_offsets);
+
+            //grid.tileIDToNeighbors_values = copyFrom.tileIDToNeighbors_values;
+            AccessTools.Property(typeof(WorldGrid), "UnsafeTileIDToNeighbors_values").SetValue(grid, copyFrom.UnsafeTileIDToNeighbors_values);
+
+            //grid.tileIDToVerts_offsets = copyFrom.tileIDToVerts_offsets;
+            AccessTools.Property(typeof(WorldGrid), "UnsafeTileIDToVerts_offsets").SetValue(grid, copyFrom.UnsafeTileIDToVerts_offsets);
+
+            //grid.averageTileSize = copyFrom.averageTileSize;
+            AccessTools.Property(typeof(WorldGrid), "AverageTileSize").SetValue(grid, copyFrom.AverageTileSize);
+
+            
+            
+            var tiles = new List<Tile>();
             ___cachedTraversalDistance = -1;
             ___cachedTraversalDistanceForStart = -1;
             ___cachedTraversalDistanceForEnd = -1;
+
+            AccessTools.Property(typeof(WorldGrid), "Tiles").SetValue(grid, tiles);
 
             copyFrom = null;
 
@@ -97,23 +115,48 @@ namespace Multiplayer.Client
 
             WorldGrid grid = __instance;
 
-            grid.tileBiome = copyFrom.tileBiome;
-            grid.tileElevation = copyFrom.tileElevation;
-            grid.tileHilliness = copyFrom.tileHilliness;
-            grid.tileTemperature = copyFrom.tileTemperature;
-            grid.tileRainfall = copyFrom.tileRainfall;
-            grid.tileSwampiness = copyFrom.tileSwampiness;
-            grid.tileFeature = copyFrom.tileFeature;
-            grid.tileRoadOrigins = copyFrom.tileRoadOrigins;
-            grid.tileRoadAdjacency = copyFrom.tileRoadAdjacency;
-            grid.tileRoadDef = copyFrom.tileRoadDef;
-            grid.tileRiverOrigins = copyFrom.tileRiverOrigins;
-            grid.tileRiverAdjacency = copyFrom.tileRiverAdjacency;
-            grid.tileRiverDef = copyFrom.tileRiverDef;
+            List<SurfaceTile> copyTiles = copyFrom.Tiles.ToList<SurfaceTile>();
+            List<SurfaceTile> gridTiles = grid.Tiles.ToList<SurfaceTile>();
+
+            for(int i = 0; i < copyTiles.Count; i++)
+            {
+                gridTiles[i].biome = copyFrom[i].biome;
+                gridTiles[i].elevation = copyFrom[i].elevation;
+                gridTiles[i].hilliness = copyFrom[i].hilliness;
+                gridTiles[i].temperature = copyFrom[i].temperature;
+                gridTiles[i].rainfall = copyFrom[i].rainfall;
+                gridTiles[i].swampiness = copyFrom[i].swampiness;
+                gridTiles[i].feature = copyFrom[i].feature;
+                //gridTiles[i].Roads = copyFrom[i].hilliness;
+                AccessTools.Property(typeof(WorldGrid), "Roads").SetValue(gridTiles[i], copyFrom[i].Roads);
+                gridTiles[i].potentialRoads = copyFrom[i].potentialRoads;
+                gridTiles[i].riverDist = copyFrom[i].riverDist;
+                //gridTiles[i].Rivers = copyFrom[i].hilliness;
+                AccessTools.Property(typeof(WorldGrid), "Rivers").SetValue(gridTiles[i], copyFrom[i].riverDist);
+                gridTiles[i].potentialRivers = copyFrom[i].potentialRivers;
+
+            }
+
+
+            //grid.tileBiome = copyFrom.tileBiome;
+            //grid.tileElevation = copyFrom.tileElevation;
+            //grid.tileHilliness = copyFrom.tileHilliness;
+            //grid.tileTemperature    = copyFrom.tileTemperature;
+            //grid.tileRainfall       = copyFrom.tileRainfall;
+            //grid.tileSwampiness     = copyFrom.tileSwampiness;
+            //grid.tileFeature        = copyFrom.tileFeature;
+            //grid.tileRoadOrigins    = copyFrom.tileRoadOrigins;
+            //grid.tileRoadAdjacency  = copyFrom.tileRoadAdjacency;
+            //grid.tileRoadDef        = copyFrom.tileRoadDef;
+            //grid.tileRiverOrigins   = copyFrom.tileRiverOrigins;
+            //grid.tileRiverAdjacency = copyFrom.tileRiverAdjacency;
+            //grid.tileRiverDef       = copyFrom.tileRiverDef;
 
             // This is plain old data apart from the WorldFeature feature field which is a reference
             // It later gets reset in WorldFeatures.ExposeData though so it can be safely copied
-            grid.tiles = copyFrom.tiles;
+            
+            //grid.tiles = copyFrom.tiles;
+            AccessTools.Property(typeof(WorldGrid), "Tiles").SetValue(grid, copyFrom.Tiles);
 
             // ExposeData runs multiple times but WorldGrid only needs LoadSaveMode.LoadingVars
             copyFrom = null;
@@ -131,7 +174,10 @@ namespace Multiplayer.Client
         {
             if (copyFrom == null) return true;
 
-            __instance.layers = copyFrom.layers;
+
+            //__instance.ayers = copyFrom.layers;
+            AccessTools.Property(typeof(WorldGrid), "AllDrawLayers").SetValue(__instance, copyFrom.AllDrawLayers);
+
             copyFrom = null;
 
             return false;
