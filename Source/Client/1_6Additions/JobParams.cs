3b74986348c22266ec75bd52a00c5bc56500baaf
coolnether123 <32144172+coolnether123@users.noreply.github.com>
2025-06-16 03:05:55 -0500

    Working on JobParams syncing

--- Source/Client/1_6Additions/JobParams.cs
+++ Source/Client/1_6Additions/JobParams.cs
@@ -4,6 +4,7 @@ using Multiplayer.API;
 using Multiplayer.Common;
 using RimWorld;
 using System.Collections.Generic;
+using System.Linq;
 using Verse;
 using Verse.AI;
 
@@ -23,11 +24,13 @@ namespace Multiplayer.Client
         public bool canBashFences;
         public HaulMode haulMode;
         public Faction lordFaction;
-        public Verb verbToUse;
-        public int takeExtraIngestibles; // Corrected from bool to int
+        public int takeExtraIngestibles;
 
         public ThinkNode jobGiver;
         public ThinkTreeDef thinkTree;
+
+        private Thing verbCaster;
+        private int verbIndex = -1;
 
         public JobParams() { }
 
@@ -37,19 +40,26 @@ namespace Multiplayer.Client
             targetA = job.targetA;
             targetB = job.targetB;
             targetC = job.targetC;
-            targetQueueA = job.targetQueueA;
-            targetQueueB = job.targetQueueB;
+            targetQueueA = job.targetQueueA?.ToList();
+            targetQueueB = job.targetQueueB?.ToList();
             count = job.count;
             playerForced = job.playerForced;
             canBashDoors = job.canBashDoors;
             canBashFences = job.canBashFences;
             haulMode = job.haulMode;
-            lordFaction = job.lord?.faction; // Corrected to use lord.faction
-            verbToUse = job.verbToUse;
-            takeExtraIngestibles = job.takeExtraIngestibles; // Corrected field
+            lordFaction = job.lord?.faction;
+            takeExtraIngestibles = job.takeExtraIngestibles;
 
             jobGiver = job.jobGiver;
             thinkTree = job.jobGiverThinkTree;
+
+            if (job.verbToUse != null)
+            {
+                verbCaster = job.verbToUse.Caster;
+                // CORRECTED: Cast to IVerbOwner to access VerbTracker
+                var owner = verbCaster as IVerbOwner;
+                verbIndex = owner?.VerbTracker?.AllVerbs.IndexOf(job.verbToUse) ?? -1;
+            }
         }
 
         public Job ToJob()
@@ -65,10 +75,21 @@ namespace Multiplayer.Client
             job.canBashDoors = canBashDoors;
             job.canBashFences = canBashFences;
             job.haulMode = haulMode;
-            job.verbToUse = verbToUse;
-            job.takeExtraIngestibles = takeExtraIngestibles; // Corrected field
+            job.takeExtraIngestibles = takeExtraIngestibles;
             job.jobGiver = jobGiver;
             job.jobGiverThinkTree = thinkTree;
+
+            if (verbCaster != null && verbIndex != -1)
+            {
+                // CORRECTED: Cast to IVerbOwner to access VerbTracker
+                var owner = verbCaster as IVerbOwner;
+                var tracker = owner?.VerbTracker;
+                if (tracker != null && verbIndex >= 0 && verbIndex < tracker.AllVerbs.Count)
+                {
+                    job.verbToUse = tracker.AllVerbs[verbIndex];
+                }
+            }
+
             return job;
         }
 
@@ -86,10 +107,12 @@ namespace Multiplayer.Client
             worker.Bind(ref canBashFences);
             worker.Bind(ref haulMode);
             worker.Bind(ref lordFaction);
-            worker.Bind(ref verbToUse);
-            worker.Bind(ref takeExtraIngestibles); // Corrected field
+            worker.Bind(ref takeExtraIngestibles);
             worker.Bind(ref jobGiver);
             worker.Bind(ref thinkTree);
+
+            worker.Bind(ref verbCaster);
+            worker.Bind(ref verbIndex);
         }
     }
 }
